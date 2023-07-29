42d4f20bd2b0585fac243df5721f28e319909c01
MuhammadSodiq <muhammadsodiqmarupov@gmail.com>
2023-07-29 14:52:13 +0500

    test

--- frontend/src/pages/universal/Table.jsx
+++ frontend/src/pages/universal/Table.jsx
@@ -42,7 +42,7 @@ const Table = ({
             className="form-select w-25"
             defaultValue={"10"}
             onChange={(e) => {
-              handlePageChange(0)
+              handlePageChange(0) 
               chageSizeOfPage({
                 api: paginationApi,
                 size: parseInt(e.target.value),
