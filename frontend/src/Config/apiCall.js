c516862f31c9b76e777ac0405beac9d00aa9e6ef
MuhammadSodiq <muhammadsodiqmarupov@gmail.com>
2023-08-05 10:25:16 +0500

    merged

--- frontend/src/Config/apiCall.js
+++ frontend/src/Config/apiCall.js
@@ -1,27 +1,17 @@
 import axios from "axios";
 
-export default function (url, method, data, searchParam) {
-  let item = localStorage.getItem("access_token");
-  return axios({
-    url: "http://localhost:8080/api" + url,
-    method: method,
-    data,
-    headers: {
-      token: item,
-      searchParam: searchParam,
-    },
-  }).catch((err) => {
-    if (err.response) {
-      if (err.response.status === 403) {
-        axios({
-          url:
-            "http://localhost:8080/api/users/refresh?refreshToken=" +
-            localStorage.getItem("refresh_token"),
-          method: "POST",
-        })
-          .then((res) => {
-            alert("olib keldim tokenni qayta")
-            localStorage.setItem("access_token", res.data);
+export default function (url, method, data,searchParam="") {
+    let item = localStorage.getItem("access_token");
+    return axios({
+        url: "http://localhost:8080/api" + url,
+        method: method,
+        data,
+        headers: {
+            "token": item,
+            "searchParam": searchParam
+        },
+    }).catch((err)=>{
+        if (err.response.status === 403){
             axios({
                 url: "http://localhost:8080/api/auth/refresh?refreshToken="+localStorage.getItem("refresh_token"),
                 method: "POST"
