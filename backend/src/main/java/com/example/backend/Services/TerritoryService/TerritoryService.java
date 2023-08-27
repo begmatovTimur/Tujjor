0dfef5008bd2199333f385bcd6aeef40ec5ad843
MuhammadSodiq <muhammadsodiqmarupov@gmail.com>
2023-08-27 21:31:37 +0500

    a

--- backend/src/main/java/com/example/backend/Services/TerritoryService/TerritoryService.java
+++ backend/src/main/java/com/example/backend/Services/TerritoryService/TerritoryService.java
@@ -1,18 +1,12 @@
 package com.example.backend.Services.TerritoryService;
 
-import com.example.backend.DTO.SearchActiveDTO;
 import com.example.backend.DTO.TerritoryDTO;
 import com.example.backend.Entity.Territory;
+import org.springframework.http.HttpEntity;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
+
 
 public interface TerritoryService {
     Territory addTerritory(TerritoryDTO territory);
